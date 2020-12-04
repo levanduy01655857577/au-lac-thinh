59fa80dc9685e9cf15c4f91a17ac99bb3b43079c
Huỳnh Tấn Thịnh <htthinh1999@gmail.com>
2020-12-04 16:47:16 +0700

    Update header, add image detail page

--- assets/js/home.js
+++ assets/js/home.js
@@ -3515,68 +3515,6 @@
 });
 // end slick slider
 
-// count up
-$(function() {
-        let visibilityIds = ['#counters_1', '#counters_2', '#counters_3'];
-        let counterClass = '.counter';
-        let defaultSpeed = 3000;
-        $(window).on('scroll', function() {
-            getVisibilityStatus();
-        });
-        getVisibilityStatus();
-
-        function getVisibilityStatus() {
-            elValFromTop = [];
-            var windowHeight = $(window).height(),
-                windowScrollValFromTop = $(this).scrollTop();
-
-            visibilityIds.forEach(function(item, index) {
-                try {
-                    elValFromTop[index] = Math.ceil($(item).offset().top);
-                } catch (err) {
-                    return;
-                }
-                if ((windowHeight + windowScrollValFromTop) > elValFromTop[index]) {
-                    counter_init(item);
-                }
-            });
-        }
-
-        function counter_init(groupId) {
-            let num, speed, direction, index = 0;
-            $(counterClass).each(function() {
-                num = $(this).attr('data-TargetNum');
-                speed = $(this).attr('data-Speed');
-                direction = $(this).attr('data-Direction');
-                easing = $(this).attr('data-Easing');
-                if (speed == undefined) speed = defaultSpeed;
-                $(this).addClass('c_' + index);
-                doCount(num, index, speed, groupId, direction, easing);
-                index++;
-            });
-        }
-
-        function doCount(num, index, speed, groupClass, direction, easing) {
-            let className = groupClass + ' ' + counterClass + '.' + 'c_' + index;
-            if (easing == undefined) easing = "swing";
-            $(className).animate({
-                num
-            }, {
-                duration: +speed,
-                easing: easing,
-                step: function(now) {
-                    if (direction == 'reverse') {
-                        $(this).text(num - Math.floor(now));
-                    } else {
-                        $(this).text(Math.floor(now));
-                    }
-                },
-                complete: doCount
-            });
-        }
-    })
-    // end count up
-
 // Srcoll top
 function showBtnScrollTop() {
     if ($('.btnScrollTop').length) {
@@ -3605,7 +3543,7 @@ function fixedMenu() {
 }
 // end fixed menu
 
-// show  menu
+// show menu
 function closeMenuHeaderMobile() {
     $('.wrapHeader').removeClass('showMenu');
     $('.wrapListMenuSubHeaderBottom .position').removeClass('smallListMenuSubHeader');
