d146dea030fb917718b7bbb6873c9f9d4a64b0e3
Huỳnh Tấn Thịnh <htthinh1999@gmail.com>
2020-12-04 15:53:34 +0700

    Add video-lists page

--- assets/js/video-lists.js
+++ assets/js/video-lists.js
@@ -146,6 +146,52 @@ window.addEventListener("load", function(event) {
     showBtnScrollTop();
 
     fixedMenu();
+    
+    // new active header (support for UU DAI, TIEC CUOI, CATERING, SU KIEN)
+    var title = sessionStorage.getItem("title");
+    var activatedTitle = $('.listItemMenuMainHeaderBottom.active').children('.listLinkMenuMainHeaderBottom').attr('title');
+    var menuLinks = ['ƯU ĐÃI', 'TIỆC CƯỚI', 'CATERING', 'SỰ KIỆN'];
+    var activated = 0;
+    $.each(menuLinks, function(key, val){
+        if(activatedTitle == val){
+            activated = 1;
+            return;
+        }
+    });
+    if((title !== null) && activated){        
+        // check if title not in menuLinks and activatedTitle in menuLinks
+        activated = 0;
+        $.each(menuLinks, function(key, val){
+            if(title == val){
+                activated = 1;
+                return;
+            }
+        });
+
+        if(activated){
+            $('.listLinkMenuMainHeaderBottom.active').removeClass('active');
+            $('.menuSubHeaderBottom.active').removeClass('active');
+            
+            $.each($('.listLinkMenuMainHeaderBottom'), function(){
+
+                if($(this).attr('title') == title){
+                    $(this).parent().addClass('active');
+                    // $('.titlePageCurrentBreadcrumb').html(title);
+                    // $('.breadcrumb-item.itemListBreadcrumb.active').html(title.toLowerCase());
+                    return;
+                }
+            });
+        }        
+    }
+
+    $('.listLinkMenuMainHeaderBottom').on('click', function() {
+        sessionStorage.setItem("title", $(this).attr('title'));
+    });
+    $('.listLinkMenuSubHeaderBottom').on('click', function(){
+        var menuItemTitle = $(this).closest('.listItemMenuMainHeaderBottom').find('.listLinkMenuMainHeaderBottom').attr('title');
+        sessionStorage.setItem("title", menuItemTitle);
+    })
+    // end new active header
 });
 
 $(window).scroll(function() {
